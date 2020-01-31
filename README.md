bec65b79914ae636dd018e91d12083ec1f2b632a
wangxianshou <wangxianshou@hyperchain.cn>
2020-01-31 14:00:16 +0800

    二叉树的遍历

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Leetcode
 
-
+1,HeapSort.Java ;堆排序及有关方法。
+2，TreeNode.Java ;二叉树的遍历
---
+++ src/com/wxs/leetcode/TreeNode.java
@@ -0,0 +1,8 @@
+package com.wxs.leetcode;/*
+ *  @author huajishaonian
+ *  time : 2020-01-2020/1/31-1:52 下午
+ *
+ */
+
+public class TreeNode {
+}
